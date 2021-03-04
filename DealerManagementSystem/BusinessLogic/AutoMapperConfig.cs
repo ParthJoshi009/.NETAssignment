b7fc8086f809c31d67908754e9e2c376fceec1a0
ParthJoshi009 <74600742+ParthJoshi009@users.noreply.github.com>
2021-03-04 23:46:04 +0530

    Added

---
+++ DealerManagementSystem/BusinessLogic/AutoMapperConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace BusinessLogic
+{
+    public static class AutoMapperConfig
+    {
+        public static IMapper Mapper { get; private set; }
+        public static void init()
+        {
+            var config = new MapperConfiguration(cfg => 
+            cfg.CreateMap<>(BusinessObjects.Dealer,DataAccess.tblDealer)
+            );
+            Mapper = config.CreateMapper();
+        }
+    }
+}
